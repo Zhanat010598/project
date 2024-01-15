6c0c3be353bfc3b357cc2222fdbd80e996c472d9
Zhanat <example@gmail.com>
2024-01-15 16:31:13 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,47 @@
+"use strict";
+ 
+// let number = 4.6;
+
+// console.log(-4/0);
+// console.log('string' * 9);
+
+// const persone = ('5');
+
+// let und;
+// console.log(und);
+
+// const obj = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// };
+// console.log(obj.name);
+
+// let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+//     console.log(arr[1]);
+
+// alert('Hello');
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = prompt("Вам есть 18?", "18");
+// console.log (answer + 5);
+
+// const answers = [];
+// // answers[0] = prompt('Как ваше имя?','');
+// // answers[1] = prompt('Как ваша фамилия', '');
+// // answers[2] = prompt('Сколько вам лет','');
+// // console.log (typeof(answers));
+// const user = "Иван";
+// alert(`Привет, ${user}`);
+// let incr = 10,
+//     decr = 10;
+// // ++incr ;
+// // --decr ;
+// console.log(++incr);
+// console.log(--decr);
+console.log(2 + 2 * 2 !== '6');
+const isChecked = true,
+    isClose = false;
+console.log(isChecked || !isClose);
